16d07b01d5cf3aacc7068464c4f0b1fef1fccd3c
GertS <gertsterenborg@gmail.com>
2016-01-07 10:09:48 +0100

    Function works now

--- main.R
+++ main.R
@@ -2,6 +2,21 @@
 #7 January 2016
 
 is.leap <- function(year=as.numeric(format(Sys.Date(), "%Y"))){
-  return(FALSE)
+  if(!is.numeric(year)){
+    stop("year is not a numeric")
+  }else if(year < 1582){
+    return(paste(year,"is out of the valid range"))
+  }else{
+    leap <- FALSE
+    if(year %% 4 == 0){
+      if(year %% 100 == 0){
+        if(year %% 400 == 0){
+          leap <- TRUE
+        }
+      }else{
+        leap <- TRUE
+      }
+    }
+    return(leap)
+  }
 }
-is.leap()
