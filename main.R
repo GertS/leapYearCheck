08846aa9e9152432fc6b3128767d0e03d8625578
GertS <gertsterenborg@gmail.com>
2016-01-07 13:46:43 +0100

    Structurize the data, create R folder and a new main.R

--- main.R
+++ main.R
@@ -1,22 +1,11 @@
 #Gert Sterenborg & Job de Pater
 #7 January 2016
 
-is.leap <- function(year=as.numeric(format(Sys.Date(), "%Y"))){
-  if(!is.numeric(year)){
-    stop("year is not a numeric")
-  }else if(year < 1582){
-    return(paste(year,"is out of the valid range"))
-  }else{
-    leap <- FALSE
-    if(year %% 4 == 0){
-      if(year %% 100 == 0){
-        if(year %% 400 == 0){
-          leap <- TRUE
-        }
-      }else{
-        leap <- TRUE
-      }
-    }
-    return(leap)
-  }
-}
+source("R/leapYearCalc.R")
+
+is.leap("nineteen-seventy-four")
+is.leap()
+is.leap(1581)
+is.leap(1582)
+is.leap(2010)
+is.leap(2000)
